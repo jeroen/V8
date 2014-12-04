43e8bc756f387774fc56cf1db87014a131c43375
Jeroen <jeroenooms@gmail.com>
2014-12-04 00:43:35 -0800

    Typo

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from curl website.
 if(!file.exists("../windows/v8-3.14.5/include/v8.h")){
   setInternet2()
-  download.file("http://jeroenooms.github.io/V8/windows/v8-3.14.5.zip", "lib.zip", quiet = TRUE)
+  download.file("http://jeroenooms.github.io/V8/v8-3.14.5.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
