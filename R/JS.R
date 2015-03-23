bd16820ea94409f87f83518e19998f23b3cccb15
Jeroen Ooms <jeroenooms@gmail.com>
2015-03-23 11:21:01 -0700

    Change I() to JS()

---
+++ R/JS.R
@@ -0,0 +1,23 @@
+#' Mark character strings as literal JavaScript code
+#'
+#' This function \code{JS()} marks character vectors with a special class, so
+#' that it will be treated as literal JavaScript code. It was copied from the
+#' htmlwidgets package, and does exactly the same thing.
+#'
+#' @param ... character vectors as the JavaScript source code (all arguments
+#'   will be pasted into one character string)
+#' @author Yihui Xie
+#' @export
+#' @examples ct <- new_context()
+#' ct$eval("1+1")
+#' ct$eval(JS("1+1"))
+#' ct$assign("test", JS("2+3"))
+#' ct$get("test")
+JS <- function(...) {
+  x <- c(...)
+  if (is.null(x)) return()
+  if (!is.character(x))
+    stop("The arguments for JS() must be a chraracter vector")
+  x <- paste(x, collapse = '\n')
+  structure(x, class = unique(c("JS_EVAL", oldClass(x))))
+}
