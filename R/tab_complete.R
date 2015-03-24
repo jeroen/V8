f9e3aeffe32d9cc840c7c7eb8b09f7e3e2753712
Jeroen Ooms <jeroenooms@gmail.com>
2015-03-23 18:25:25 -0700

    Minor enhancements

--- R/tab_complete.R
+++ R/tab_complete.R
@@ -1,5 +1,11 @@
 #' @importFrom utils rc.options
 tab_complete <- function(ct, buf){
+  # Check if this is a completable expression
+  if(!ct$validate(paste0(buf, "xx"))){
+    return(character())
+  }
+
+  # Buffer ends with a dot
   if(substr(buf, nchar(buf), nchar(buf)) == "."){
     base <- substr(buf, 1, nchar(buf)-1)
     token <- ""
@@ -8,7 +14,7 @@ tab_complete <- function(ct, buf){
     token <- tail(strsplit(buf, ".", fixed=T)[[1]], 1)
   }
   object <- paste(c("this", base), collapse=".")
-  tryCatch({
+  comps <- tryCatch({
     fields <- ct$call("Object.getOwnPropertyNames", JS(object))
     suggestions <- grep(paste0("^", token, "."), fields, value = TRUE)
     if(length(base) && length(suggestions)) {
@@ -17,4 +23,5 @@ tab_complete <- function(ct, buf){
       suggestions
     }
   }, error = function(e) character());
+  sort(comps)
 }
