b3f4e455988aaccfedd3c88ab34ffe94aac9f216
unknown <jeroenooms@gmail.com>
2015-08-03 13:50:09 +0200

    Update windows libv8 to 3.14.5.10

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from curl website.
-if(!file.exists("../windows/libv8-3.14.5/include/v8.h")){
+if(!file.exists("../windows/libv8-3.14.5.10/include/v8.h")){
   setInternet2()
-  download.file("https://github.com/rwinlib/libv8/archive/v3.14.5.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libv8/archive/v3.14.5.10.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
