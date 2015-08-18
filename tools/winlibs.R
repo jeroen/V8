f0931c275867760d3a5be9287175ac620d56f67f
unknown <jeroenooms@gmail.com>
2015-08-18 20:22:57 +0200

    Fix for R 3.3

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
 # Build against static libraries from curl website.
 if(!file.exists("../windows/libv8-3.14.5.10/include/v8.h")){
-  setInternet2()
+  if(getRversion() <= "3.2.2") setInternet2()
   download.file("https://github.com/rwinlib/libv8/archive/v3.14.5.10.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
