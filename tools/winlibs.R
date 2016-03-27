72542185c717f0017534321c4a2b3581f31c7931
Jeroen Ooms <jeroenooms@gmail.com>
2016-03-28 01:38:10 +0200

    Typos

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,5 +1,5 @@
 # Build against mingw-w64 build of libv8 3.15.10
-if(!file.exists("../windows/libv8-3.15/include/v8.h")){
+if(!file.exists("../windows/libv8-3.15.10/include/v8.h")){
   if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/libv8/archive/v3.15.10.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
