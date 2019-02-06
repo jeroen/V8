6011919df0addd331db8f01f6b749d572a4a95e0
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-06 06:26:00 +0100

    Temp fix

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,7 +3,7 @@ VERSION <- commandArgs(TRUE)
 LIBDIR <- paste0("../windows/libv8-", VERSION)
 if(!file.exists(paste0(LIBDIR, "/include/v8.h"))){
   if(getRversion() < "3.3.0") stop("This library requires R 3.3 or newer")
-  download.file(sprintf("https://github.com/rwinlib/libv8/archive/v%s.zip", VERSION), "v8.zip", quiet = TRUE)
+  download.file(sprintf("https://github.com/rwinlib/libv8/archive/v%s.zip", VERSION), "v8.zip", quiet = TRUE, method = 'libcurl')
   dir.create("../windows", showWarnings = FALSE)
   unzip("v8.zip", exdir = "../windows")
   setwd(LIBDIR)
