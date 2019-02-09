6cf9cfd23bf1b1082a13e51ec95558cad09c5668
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-09 12:32:06 +0100

    Add comment

--- tests/testthat/test_exception.R
+++ tests/testthat/test_exception.R
@@ -1,5 +1,7 @@
 context("Error message")
 
+# This test fails with the CRAN custom clang4 compiler.
+# The test is automatically removed by the autobrew script
 test_that("SyntaxError from V8", {
   ctx <- V8::v8()
   expect_error(ctx$eval('var foo = }bla}'), 'SyntaxError')
