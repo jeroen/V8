95b6d361986c1c95da660969b5196e7a3d097849
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2020-04-18 13:51:47 +0200

    cleanup and check if any output program was compiled at all if not fallback to default

--- inst/test.cpp
+++ inst/test.cpp
@@ -1,15 +1,6 @@
-// #include <iostream>
 #include <v8.h>
 
 int main()
 {
-  // try {
-  //   v8::V8::Initialize();
-  //   }
-  // catch(...) {
-  //   std::cerr << "1" << std::endl;
-  //   };
-
   v8::V8::Initialize();
-
 }
