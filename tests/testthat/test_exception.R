b50b6f465141a0f5cc368cbda7f602d47c05c966
Jeroen Ooms <jeroenooms@gmail.com>
2020-07-21 14:35:34 +0200

    Do not skip test on R 4

--- tests/testthat/test_exception.R
+++ tests/testthat/test_exception.R
@@ -4,6 +4,7 @@ context("Error message")
 # The test is automatically removed by the autobrew script
 test_that("SyntaxError from V8", {
   ctx <- V8::v8()
-  skip_on_os("mac")
+  if(getRversion() < 4)
+    skip_on_os("mac")
   expect_error(ctx$eval('var foo = }bla}'), 'SyntaxError', class = "std::invalid_argument")
 })
