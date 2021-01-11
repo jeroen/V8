a6f9c9e1e067b01daf7a28331896eb840ea77d7e
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-11 15:26:41 +0100

    add support for musl

--- get-v8-linux.sh
+++ get-v8-linux.sh
@@ -1,8 +1,13 @@
-IS_GCC4=$($CXX --version | grep -P '^g++.*[^\d.]4(\.\d){2}')
-if [ $? -ne 0 ] || [ -z "$IS_GCC4" ]; then
-  URL="http://jeroen.github.io/V8/v8-8.3.110.13-linux.tar.gz"
+IS_MUSL=$(ldd --version 2>&1 | grep musl)
+if [ $? -eq 0 ] && [ "$IS_MUSL" ]; then
+  URL="http://jeroen.github.io/V8/v8-8.3.110.13-musl.tar.gz"
 else
-  URL="http://jeroen.github.io/V8/v8-6.8.275.32-gcc48.tar.gz"
+  IS_GCC4=$($CXX --version | grep -P '^g++.*[^\d.]4(\.\d){2}')
+  if [ $? -eq 0 ] && [ "$IS_GCC4" ]; then
+    URL="http://jeroen.github.io/V8/v8-6.8.275.32-gcc48.tar.gz"
+  else
+    URL="http://jeroen.github.io/V8/v8-8.3.110.13-linux.tar.gz"
+  fi
 fi
 ${R_HOME}/bin/R -q -e "curl::curl_download('$URL','libv8.tar.gz',quiet=FALSE)"
 tar xzf libv8.tar.gz
