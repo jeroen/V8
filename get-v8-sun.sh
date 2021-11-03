4725861252e4ec8c0e89ac2cdc2094af1de083b8
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-03 11:04:55 +0100

    update solaris v8

--- get-v8-sun.sh
+++ get-v8-sun.sh
@@ -1,11 +1,11 @@
 LIBDIR="$PWD/.deps"
-url="http://jeroen.github.io/V8/v8-6.8.275.32-sun32.tar.gz"
+url="http://jeroen.github.io/V8/v8-7.0.276.38-sun32.tar.gz"
 mkdir -p $LIBDIR
 curl -sSL $url -o libs.tar.gz
 if [ -f "libs.tar.gz" ]; then
   gtar zxf libs.tar.gz -C $LIBDIR
   rm -f libs.tar
-  V8PATH="$LIBDIR/v8-6.8.275.32-sun32"
+  V8PATH="$LIBDIR/v8-7.0.276.38-sun32"
   if [ -f "${V8PATH}/lib/libv8_base.a" ]; then
     PKG_CFLAGS="-I${V8PATH}/include -DV8_ENABLE_CHECKS"
     PKG_LIBS="-L${V8PATH}/lib -lv8_base -lv8_libplatform  -lv8_nosnapshot -lv8_libbase -lv8_libsampler -lv8_init -lv8_initializers"
