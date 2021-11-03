b0e1c165bd6394c143991db79a4950b88ff00f51
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-03 16:19:19 +0100

    use snapshot on solaris, much faster

--- get-v8-sun.sh
+++ get-v8-sun.sh
@@ -8,7 +8,7 @@ if [ -f "libs.tar.gz" ]; then
   V8PATH="$LIBDIR/v8-7.0.276.38-sun32"
   if [ -f "${V8PATH}/lib/libv8_base.a" ]; then
     PKG_CFLAGS="-I${V8PATH}/include -DV8_ENABLE_CHECKS"
-    PKG_LIBS="-L${V8PATH}/lib -lv8_base -lv8_libplatform  -lv8_nosnapshot -lv8_libbase -lv8_libsampler -lv8_init -lv8_initializers"
+    PKG_LIBS="-L${V8PATH}/lib -lv8_base -lv8_libplatform  -lv8_snapshot -lv8_libbase -lv8_libsampler -lv8_init -lv8_initializers"
     sed -e "s|@cflags@|$PKG_CFLAGS|" -e "s|@libs@|$PKG_LIBS|" src/Makevars.in > src/Makevars
     exit 0
   fi
