3234c16af91bad997bc7b61a5488b0003ee09c73
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-05 11:24:59 +0100

    Minor tweaks

--- tests/testthat/test_wasm.R
+++ tests/testthat/test_wasm.R
@@ -1,7 +1,7 @@
 context("WASM")
 
 test_that("Load WASM program", {
-  skip_if_not(engine_info()$version > 6)
+  skip_if_not(engine_info()$version > 6, message = "Your libv8 is too old for WASM")
   instance <- wasm(system.file('wasm/add.wasm', package = 'V8'))
   expect_equal(names(instance$exports), 'add')
   expect_equal(instance$exports$add(12, 30), 42)
