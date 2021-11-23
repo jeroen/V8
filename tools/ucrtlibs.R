749a14a5ae3a1394617eb9dca32eb7d037d7b8d3
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-23 23:04:22 +0100

    Windows ucrt: update libv8 to 9.1.269.38

--- tools/ucrtlibs.R
+++ tools/ucrtlibs.R
@@ -1,6 +1,6 @@
 # Build against UCRT build of libv8
 if(!file.exists('../windows/ucrt64/include/v8.h')){
-  download.file('https://github.com/r-windows/rtools-packages/releases/download/v8-9.0.257.17/ucrt64.tar.xz', "ucrt64.tar.xz", quiet = TRUE)
+  download.file('https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-ucrt.pkg.tar.xz', "ucrt64.tar.xz", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   untar('ucrt64.tar.xz', exdir = "../windows")
   unlink('ucrt64.tar.xz')
