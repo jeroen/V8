b6a68f63c1b7629ea03ed8ccf98fa59a4617cc2b
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-25 15:27:13 +0100

    Try with tar internal to ensure xz support

--- tools/ucrtlibs.R
+++ tools/ucrtlibs.R
@@ -2,6 +2,6 @@
 if(!file.exists('../windows/ucrt64/include/v8.h')){
   download.file('https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-ucrt.pkg.tar.xz', "ucrt64.tar.xz", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  untar('ucrt64.tar.xz', exdir = "../windows")
+  untar('ucrt64.tar.xz', exdir = "../windows", tar = 'internal')
   unlink('ucrt64.tar.xz')
 }
