f82b9dcc34ec7b54c64fefcc27d25ce3eaacc8ec
Jeroen Ooms <jeroenooms@gmail.com>
2021-12-22 20:05:02 +0100

    Don't longjump from R when running in V8

--- R/callback.R
+++ R/callback.R
@@ -2,35 +2,47 @@
 #
 # Provides: console.r.call("rnorm", {n:10})
 r_call <- function(strfun, args = '{}'){
-  FUN <- eval(parse(text=strfun))
-  ARGS <- as.list(jsonlite::fromJSON(args))
-  if(!is.function(FUN))
-    stop("Argument is not a valid function")
-  out <- do.call(FUN, ARGS)
-  jsonlite::toJSON(out)
+  no_jumps({
+    FUN <- eval(parse(text=strfun))
+    ARGS <- as.list(jsonlite::fromJSON(args))
+    if(!is.function(FUN))
+      stop("Argument is not a valid function")
+    out <- do.call(FUN, ARGS)
+    jsonlite::toJSON(out)
+  })
 }
 
 # Provides: console.r.get("iris")
 r_get <- function(str, args = '{}'){
-  x <- eval(parse(text = str))
-  ARGS <- as.list(jsonlite::fromJSON(args))
-  do.call(jsonlite::toJSON, c(list(x = x), ARGS))
+  no_jumps({
+    x <- eval(parse(text = str))
+    ARGS <- as.list(jsonlite::fromJSON(args))
+    do.call(jsonlite::toJSON, c(list(x = x), ARGS))
+  })
 }
 
 # Provides: console.r.eval("rnorm(10)")
 r_eval <- function(str, args = '{"print.eval":true}'){
-  con <- textConnection(str)
-  ARGS <- as.list(jsonlite::fromJSON(args))
-  out <- do.call(source, c(list(file = con), ARGS))
-  #tryCatch(toJSON(out$value), error = 'null')
-  return('null')
+  no_jumps({
+    con <- textConnection(str)
+    ARGS <- as.list(jsonlite::fromJSON(args))
+    do.call(source, c(list(file = con), ARGS))
+    #tryCatch(toJSON(out$value), error = 'null')
+    return('null')
+  })
 }
 
 # Provides: console.r.assign("test", [1,2,3])
 r_assign <- function(name, value, args = '{}'){
-  ARGS <- as.list(jsonlite::fromJSON(args))
-  VAL <- do.call(jsonlite::fromJSON, c(list(txt = value), ARGS))
-  asgn <- get("assign", "package:base")
-  asgn(name, VAL, globalenv())
-  return('null')
+  no_jumps({
+    ARGS <- as.list(jsonlite::fromJSON(args))
+    VAL <- do.call(jsonlite::fromJSON, c(list(txt = value), ARGS))
+    asgn <- get("assign", "package:base")
+    asgn(name, VAL, globalenv())
+    return('null')
+  })
+}
+
+no_jumps <- function(...){
+  tryCatch(..., interrupt = function(e){stop("Interruption")})
 }
