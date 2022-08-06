5cfa51f6441cfcc2acd623c6ffe5a9955da486f7
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-06 16:01:32 +0200

    Make feature test more robust

--- tools/test.cpp
+++ tools/test.cpp
@@ -3,7 +3,9 @@
 using namespace v8;
 
 int main(){
+#if (V8_MAJOR_VERSION * 100 + V8_MINOR_VERSION) >= 704
   std::unique_ptr<Platform> platform = platform::NewDefaultPlatform();
   V8::InitializePlatform(platform.get());
+#endif
   V8::Initialize();
 }
