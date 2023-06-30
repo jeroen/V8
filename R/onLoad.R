dda2f77d3333a513750a6834ac66a42a3343fff6
Jeroen Ooms <jeroenooms@gmail.com>
2023-06-30 11:37:59 +0200

    Fix another version comparison issue

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkg){
   ver <- version()
   packageStartupMessage(paste("Using V8 engine", ver))
-  if(isTRUE(v8_version_numeric() < 6)){
+  if(isTRUE(v8_version_numeric() < "6")){
     warning("This system has a very old version of libv8. Some packages may not work.", call. = FALSE)
   }
 }
