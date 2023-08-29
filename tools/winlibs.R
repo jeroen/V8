81d346a276aa721b32c6b5417a052b13ee39b43b
Jeroen Ooms <jeroenooms@gmail.com>
2023-08-29 14:41:28 +0200

    Windows (R-4.3 and up): update libv8 to 11.8

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,9 +3,13 @@ if(grepl('Windows Server 2008', osVersion)){
   stop("Windows Vista/2008 is no longer supported")
 }
 if(!file.exists('../windows/ucrt64/include/v8.h')){
-  cdn <- ifelse(isTRUE(grepl('ucrt', R.version$crt)),
+  cdn <- if(getRversion() >= "4.3.0"){
+    'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-11.8.7-ucrt.tar.xz'
+  } else {
+    ifelse(isTRUE(grepl('ucrt', R.version$crt)),
            'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-ucrt.pkg.tar.xz',
            'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-msvcrt.tar.xz')
+  }
   download.file(cdn, "libv8.tar.xz", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   untar('libv8.tar.xz', exdir = "../windows", tar = 'internal')
