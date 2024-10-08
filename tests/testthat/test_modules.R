37df5d00a396cb3f3188e0ebeed9ac05f1c559b8
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-08 14:22:23 +0200

    Repeat tests

--- tests/testthat/test_modules.R
+++ tests/testthat/test_modules.R
@@ -4,9 +4,8 @@ test_that("get modules", {
   skip_if(V8::engine_info()$numeric_version < "6.3")
   ctx <- V8::v8()
   ctx$source('modules/main.js')
-  out <- ctx$eval('run_test()', await = TRUE)
   expect_error(ctx$eval('test_bad_path()', await = TRUE), "path")
-  expect_equal(out, "579")
+  expect_equal(ctx$eval('run_test()', await = TRUE), "579")
   expect_error(ctx$eval('test_syntax_error1()', await = TRUE), "SyntaxError")
-
+  expect_equal(ctx$eval('run_test()', await = TRUE), "579")
 })
