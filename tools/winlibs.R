6afaac219c472c0ce2ff7abdc00c9fba4e812e97
Jeroen Ooms <jeroenooms@gmail.com>
2025-03-14 23:24:22 +0100

    Windows: minor update of libv8 to fix a warning with rtools45

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists('../windows/libv8/include/v8.h')){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/v8-11.8.172.13/v8-11.8.172.13-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/v8-11.9.169.6/v8-11.9.169.6-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/v8-11.8.172.13/v8-11.8.172.13-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/v8-11.9.169.6/v8-11.9.169.6-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
-    "https://github.com/r-windows/bundles/releases/download/v8-11.8.172.13/v8-11.8.172.13-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/v8-11.9.169.6/v8-11.9.169.6-ucrt-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
     "https://github.com/r-windows/bundles/releases/download/v8-11.8.172.13/v8-9.1.269.38-win-ucrt.tar.xz"
   } else {
