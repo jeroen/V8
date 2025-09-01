d4358a969f6c9080cca3c6718e69fc3509271f02
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-01 08:22:59 +0200

    Static library for Windows update

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists('bindings.o') && !file.exists('../.deps/libv8/include/v8.h')){
   unlink("../.deps", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/v8-11.9.169.6/v8-11.9.169.6-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/v8-14.0.365.4/v8-14.0.365.4-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/v8-11.9.169.6/v8-11.9.169.6-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/v8-14.0.365.4/v8-14.0.365.4-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
-    "https://github.com/r-windows/bundles/releases/download/v8-11.9.169.6/v8-11.9.169.6-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/v8-14.0.365.4/v8-14.0.365.4-ucrt-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
     "https://github.com/r-windows/bundles/releases/download/v8-11.8.172.13/v8-9.1.269.38-win-ucrt.tar.xz"
   } else {
