c988261471aa061458bd28af2cdb772d784a7999
George Stagg <georgestagg@gmail.com>
2025-09-26 18:50:43 +0100

    Initial support for webR using Emscripten JS APIs and Web Workers (#194)

--- src/V8_types.h
+++ src/V8_types.h
@@ -1,11 +1,17 @@
-#include <v8.h>
 #include <Rcpp.h>
+
+#ifndef __EMSCRIPTEN__
+#include <v8.h>
 
 #if (V8_MAJOR_VERSION * 100 + V8_MINOR_VERSION) >= 1001
 typedef v8::Global<v8::Context> ctx_type;
 #else
 typedef v8::Persistent<v8::Context> ctx_type;
 #endif
+
+#else
+typedef int ctx_type;
+#endif // __EMSCRIPTEN__
 
 // typedef Rcpp::XPtr< ctx_type > v8_xptr;
 void ctx_finalizer(ctx_type* ctx);
